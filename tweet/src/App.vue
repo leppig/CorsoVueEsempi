fc9c434b1de34f21a00b4622a8bd5021e835002f
Leppig <leppig@gmail.com>
2019-06-14 08:22:34 +0200

    due
    due

--- tweet/src/App.vue
+++ tweet/src/App.vue
@@ -1,26 +1,48 @@
 <template>
+
   <div id="app">
-    <ItemList name="Tweets" :items='tweets'/>
+    <h1>Tweet</h1>
+<br>
+    <BottoniEspandi name='espandi' />
+    <Colonne name='colonne' />
+      <br>
+    <Table name="Tweets" :items='tweets'/>
   </div>
 </template>
 
 <script>
-import ItemList from './components/ItemList.vue'
+import Table from './components/Table.vue'
 import {TWEETS} from './components/tweets.js'
+import Colonne from './components/Colonne.vue'
+import BottoniEspandi from './components/BottoniEspandi.vue'
+
 
 export default {
   name: 'app',
+
   data() {
     return TWEETS;
   },
-  components: {
-    ItemList,
+  props: {
+
   },
+  components: {
+    Table,
+    Colonne,
+    BottoniEspandi
+  },
+
+  methods: {
+      toggle(d){
+         return d ? 'color: red;' : 'color: gray;';
+      },
+  }
 }
 </script>
 
 <style>
 #app {
   margin: 60px;
+
 }
 </style>
